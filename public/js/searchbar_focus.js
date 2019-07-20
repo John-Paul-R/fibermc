f5652f726f52ccd5081dc3b9b902140244f09302
John-Paul-R <johnpaul122jpr@gmail.com>
2019-07-20 05:09:29 -0400

    Search bar modifications   - Increased size of elements making it up   - Changed animation to start scaled down (.95), then scale to normal (1) when focused, rather than starting scaled normally (1), then scaling up (1.05). This eliminated blurriness that came from upscaling.

--- public/js/searchbar_focus.js
+++ public/js/searchbar_focus.js
@@ -2,12 +2,13 @@ function searchbarFocus(val) {
     parent = event.target.parentElement
     newOpacity = 1;
     if (val == 'in') {
-        parent.style.getPropertyValue('--default-scale');
-        TweenMax.to(parent, .25, { scale:1.1 });
+        TweenMax.to(parent, .25, { scale:1 });
     } else {
         if (event.target.parentElement.querySelector(".searchField").value == "") {
             newOpacity = 0.6
-            TweenMax.to(parent, .25, { scale:1 });
+            var default_scale = parseInt(parent.style.getPropertyValue('--default-scale'));
+            TweenMax.to(parent, .25, { scale: .95 });
+            
         }
 
     }
