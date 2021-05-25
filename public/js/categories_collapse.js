6eaf021885f22b9c109674040346fc4d7e549d80
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-05-24 23:08:54 -0400

    Make entire "Categories" head clickable on mobile.

--- public/js/categories_collapse.js
+++ public/js/categories_collapse.js
@@ -1,6 +1,6 @@
 
 (function() {
-    const toggle_btn = document.getElementById("categories_list_toggle");
+    const toggle_btn = document.getElementById("cat_head");
     const categories = document.getElementById("categories");
     toggle_btn.addEventListener('click', (e) => {
         categories.classList.toggle('open');
