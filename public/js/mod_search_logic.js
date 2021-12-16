0394b18fcf33139efea9e6116477dcba93129442
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-12-15 22:16:05 -0500

    I have brain.

--- public/js/mod_search_logic.js
+++ public/js/mod_search_logic.js
@@ -28,7 +28,7 @@ export {
 // Load mod data from external file
 var loader = new AsyncDataResourceLoader({
     completionWaitForDCL: true
-}).addResource('https:/dev.fibermc.com/api/v1.0/Mods', [
+}).addResource('https://dev.fibermc.com/api/v1.0/Mods', [
     (jsonData) => {
         // const temp_mods = jsonData.mods;
         console.log("TEMP", jsonData)
@@ -42,7 +42,7 @@ var loader = new AsyncDataResourceLoader({
 
         initSortCache(mod_data);
     }
-]).addResource('https://localhost:5001/api/v1.0/Categories', [
+]).addResource('https://dev.fibermc.com/api/v1.0/Categories', [
     (jsonData) => {
         setCategories(jsonData);
 
