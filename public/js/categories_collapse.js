1335b01c2df526c994b0a218efe8fd8ce466b6d7
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-06-23 20:36:12 -0400

    fix media query on safari

--- public/js/categories_collapse.js
+++ public/js/categories_collapse.js
@@ -1,12 +1,19 @@
 
-(function() {
+(function () {
     const toggle_btn = document.getElementById("cat_head");
     const categories = document.getElementById("categories");
     let isMobileQuery = window.matchMedia("(max-width: 1162px)");
     let isMobile = isMobileQuery.matches;
-    isMobileQuery.addEventListener('change', (e) => {
+    const changeHandler = (e) => {
         isMobile = e.matches;
-    });
+    };
+    if (isMobileQuery.addEventListener) {
+        isMobileQuery.addEventListener('change', changeHandler);
+    } else if (isMobileQuery.addListener) {
+        // Safari bad
+        isMobileQuery.addListener((list, event) => changeHandler(event))
+    }
+
     toggle_btn.addEventListener('click', (e) => {
         if (isMobile)
             categories.classList.toggle('open');
